3203d91ea9dde2738515a1946bea775691f478d6
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2020-09-10 18:53:50 +0800

    Update header.php

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -190,7 +190,7 @@
                         <a href="#components" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-box"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
-                                <span>Components</span>
+                                <span>Transaksi Data</span>
                             </div>
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
@@ -198,37 +198,40 @@
                         </a>
                         <ul class="collapse submenu list-unstyled" id="components" data-parent="#accordionExample">
                             <li>
-                                <a href="component_tabs.html"> Tabs </a>
+                                <a href="barang_masuk.php"> Barang Masuk </a>
                             </li>
                             <li>
-                                <a href="component_accordion.html"> Accordions  </a>
+                                <a href="barang_keluar.php"> Barang Keluar  </a>
                             </li>
-                            <li>
-                                <a href="component_modal.html"> Modals </a>
-                            </li> 
                         </ul>
                     </li>
 
                     <li class="menu">
-                        <a href="#elements" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        <a href="#forms" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-zap"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg>
-                                <span>Elements</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
+                                <span>Inventarisasi</span>
                             </div>
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
                             </div>
                         </a>
-                        <ul class="collapse submenu list-unstyled" id="elements" data-parent="#accordionExample">
+                        <ul class="collapse submenu list-unstyled" id="forms" data-parent="#accordionExample">
                             <li>
-                                <a href="element_alerts.html"> Alerts </a>
+                                <a href="kib_a.php"> KIB-A : Tanah </a>
                             </li>
                             <li>
-                                <a href="element_avatar.html"> Avatar </a>
+                                <a href="kib_b.php"> KIB-B : Peralatan dan Mesin </a>
                             </li>
                             <li>
-                                <a href="element_badges.html"> Badges </a>
+                                <a href="kib_c.php"> KIB-C : Gedung dan Bangunan </a>
                             </li>
+                            <li>
+                                <a href="kib_d.php"> KIB-D : Jalan, Irigasi dan Jaringan </a>
+                            </li>
+                            <li>
+                                <a href="kib_e.php"> KIB-E : Aset Tetap Lainnya </a>
+                            </li>                      
                         </ul>
                     </li>
 
